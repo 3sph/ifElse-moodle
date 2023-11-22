d4e92be2caf8b4d0239089d2264f1910cfb81c74
3sph <141910502+3sph@users.noreply.github.com>
2023-11-22 19:15:01 +0100

    Initial commit

---
+++ if_test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace if_test
+{
+    internal class IfElse
+    {
+        int number = 5;
+        int number2 = 1;
+        bool isEqual = false;
+
+        public void run()
+        {
+            if (number == number2)
+            {
+                isEqual = true;
+                Console.WriteLine("numbers are equal");
+            }
+            else
+            {
+                isEqual = false;
+                Console.WriteLine("numbers are not equal");
+            }
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            IfElse ifElse = new IfElse();
+            ifElse.run();
+        }
+    }
+}
